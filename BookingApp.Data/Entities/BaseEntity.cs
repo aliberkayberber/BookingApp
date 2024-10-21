be9d200518183c610596760bd547cc0fe0c3555c
Ali Berkay BERBER <aliberkayberber@gmail.com>
2024-10-22 01:16:26 +0300

    Entity and Context added with configuration

---
+++ BookingApp.Data/Entities/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Data.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+
+    public abstract class BaseConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            // bütün linq sorgularında geçerli bir filitre
+            builder.HasQueryFilter(x => x.IsDeleted == false);
+
+            builder.Property(x => x.ModifiedDate).IsRequired().IsRequired(false);
+        }
+    }
+}
---
+++ BookingApp.Data/Entities/HotelFeatureEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Data.Entities
+{
+    public class HotelFeatureEntity: BaseEntity
+    {
+        public int HotelId { get; set; }
+
+        public int FeatureId { get; set; }
+
+        public HotelEntity Hotel { get; set; }
+
+        public FeatureEntity Feature { get; set; }
+    }
+
+    public class HotelFeatureConfiguration : BaseConfiguration<HotelFeatureEntity>
+    {
+        public override void Configure(EntityTypeBuilder<HotelFeatureEntity> builder)
+        {
+            builder.Ignore(x => x.Id); // ıd propertysi görmezden geliyoruz
+
+            builder.HasKey("HotelId", "FeatureId"); // composite key oluşturuyoruz
+
+            base.Configure(builder);
+        }
+    }
+
+}
