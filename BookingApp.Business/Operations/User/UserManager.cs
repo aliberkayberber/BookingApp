17c1524812be3d4f3a61dcdc582fdde872ba0744
Ali Berkay BERBER <aliberkayberber@gmail.com>
2024-10-23 02:54:27 +0300

    Unit of work patter added

---
+++ BookingApp.Business/Operations/User/UserManager.cs
@@ -0,0 +1,70 @@
+﻿using BookingApp.Business.DataProtection;
+using BookingApp.Business.Operations.User.Dtos;
+using BookingApp.Business.Types;
+using BookingApp.Data.Entities;
+using BookingApp.Data.Repositories;
+using BookingApp.Data.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Business.Operations.User
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepository<UserEntity> _userRepository;
+        private readonly IDataProtection _dataProtection;
+
+
+        public UserManager(IUnitOfWork unitOfWork, IRepository<UserEntity> userRepository, IDataProtection dataProtection)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = userRepository;
+            _dataProtection = dataProtection;
+        }
+
+
+        public async Task<ServiceMessage> AddUser(AddUserDto user)
+        {
+            var hasMail = _userRepository.GetAll(x => x.Email.ToLower() == user.Email.ToLower());
+
+            if (hasMail.Any())
+            {
+                return new ServiceMessage
+                {
+                    IsSucceed = false,
+                    Message = "Email adresi zaten mevcut"
+                };
+            }
+
+            var userEntity = new UserEntity()
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Password =  _dataProtection.Protect(user.Password), 
+                BirthDate = user.BirthDate,
+            };
+
+            _userRepository.Add(userEntity);
+
+            try
+            {
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Kullanıcı kaydı sırasında bir hata oluştu");
+            }
+
+            return new ServiceMessage
+            {
+                IsSucceed = true,
+            };
+
+        }
+    }
+}
--- BookingApp.WebApi/Program.cs
+++ BookingApp.WebApi/Program.cs
@@ -1,6 +1,9 @@
+using BookingApp.Business.DataProtection;
+using BookingApp.Business.Operations.User;
 using BookingApp.Data.Context;
 using BookingApp.Data.Repositories;
 using BookingApp.Data.UnitOfWork;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +24,17 @@ builder.Services.AddDbContext<BookingAppDbContext>(options =>
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>)); // generic olduðu için typeof kullandýk
 
 builder.Services.AddScoped<IUnitOfWork , UnitOfWork>();
+
+builder.Services.AddScoped<IUserService , UserManager>();
+
+builder.Services.AddScoped<IDataProtection , DataProtection>();
+
+var keysDirectory = new DirectoryInfo(Path.Combine(builder.Environment.ContentRootPath, "App_Data", "Keys"));
+
+builder.Services.AddDataProtection()
+       .SetApplicationName("BookingApp")
+       .PersistKeysToFileSystem(keysDirectory);
+
 
 var app = builder.Build();
 
